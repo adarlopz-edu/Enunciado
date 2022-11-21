403fa005753f58d9e0fd5ee0c3ccd67bf3d379ee
braad <braad@A22100190>
2022-11-21 13:22:10 -0600

    Agregar archivos de proyecto.

---
+++ Enunciado/Enunciado.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char e[20];
+    int x;
+
+    cout << "Escribe un enunciado (maximo 20 caractres):\n";
+    cin.getline(e, 20) ;
+
+    x = 0;
+    cout << e[0] << "\n";
+    while ('\x0' != e[x]) {
+        x = x + 1;
+        cout << e[x] << "\n";
+   
+    }
+}
